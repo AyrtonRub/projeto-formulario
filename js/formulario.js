91db310d573f5837896bd2f0602641093574f358
Ayrton Rubens <129620321+AyrtonRub@users.noreply.github.com>
2024-06-07 20:57:19 -0300

    z

--- js/formulario.js
+++ js/formulario.js
@@ -6,25 +6,61 @@
     
     formulario.addEventListener('submit', function(e) {
         if(textoInput.value ===""){ 
-            alert('digite um valor')
+            showErrorMessage('Prencha todos os campos')
             textoInput.focus()
         }
         e.preventDefault() /*Previne o comportamento padrão*/
     })
+
+    // Função para mostrar a mensagem na tela
+
+    const feedbackMessage = document.querySelector('#feedbackMessage')
+    const mensageErro = document.getElementsByTagName('p')[0]
+    const closeBtn = document.querySelector('#close')
+    console.log(closeBtn)
+
+    function showErrorMessage(msg) {
+        feedbackMessage.classList.add('show')
+        mensageErro.textContent = msg
+
+        // Função para fechar a mensagem de erro
+        function hideErrorMessage() {
+            feedbackMessage.classList.remove('show')
+            closeBtn.removeEventListener('click', hideErrorMessage)
+            closeBtn.removeEventListener('keyup', fecharComEsc)
+        }
+
+        function fecharComEsc(e) {
+            if(e.keyCode === 27) {
+                hideErrorMessage()
+            }
+        }
+
+        closeBtn.addEventListener('click', hideErrorMessage)
+        closeBtn.addEventListener('keyup', fecharComEsc)
+    }
+
+    
+
+    
 
     // Função para cálculo dos caracters digitados no textarea
     const contadorStrings = document.querySelector('.contadorStrings span')
     const contentContador = document.querySelector('.contadorStrings')
     const textArea = document.querySelector('textarea')
     let maxima = textArea.maxLength
-    contadorStrings.textContent = maxima
+    mostrarValor(maxima)
   
     
     textArea.addEventListener('input', e => {
         let letrasDigitadas = textArea.value.length
         let caractersRestantes = parseInt(maxima) - parseFloat(letrasDigitadas)
-        contadorStrings.textContent = caractersRestantes
+        mostrarValor(caractersRestantes)
     })
+
+    function mostrarValor(n) {
+        contadorStrings.textContent = n
+    }
 
     // função para habilitar o botão se submit
 
